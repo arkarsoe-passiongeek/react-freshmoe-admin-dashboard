407f30c5bea002efb45c548a5ab826353ec4cb0b
arkarsoe <arkarsoe.passiongeek@gmail.com>
2025-01-14 13:33:13 +0630

    🐞 fix(logout): update
    logout is not working

--- src/services/apis/auth.ts
+++ src/services/apis/auth.ts
@@ -1,15 +1,17 @@
-import axios from 'axios'
+// import axios from 'axios'
 import Cookies from 'js-cookie'
 
 export const logout = async () => {
     console.log('logout')
-    const token = decodeURIComponent(Cookies.get('token') ?? '')
-    console.log(token)
-    await axios.post(`https://dev-api.freshmoe.com/api/admin/logout`, undefined, {
-        withCredentials: true, headers: {
-            'Authorization': `Bearer ${token}`,
-        },
-    })
+    // this is commented assuming that backend doesn't do anything
+
+    // const token = decodeURIComponent(Cookies.get('token') ?? '')
+    // console.log(token)
+    // await axios.post(`https://dev-api.freshmoe.com/api/admin/logout`, undefined, {
+    //     withCredentials: true, headers: {
+    //         'Authorization': `Bearer ${token}`,
+    //     },
+    // })
     localStorage.removeItem('userdata')
     Cookies.remove('token')
     return 'success'
