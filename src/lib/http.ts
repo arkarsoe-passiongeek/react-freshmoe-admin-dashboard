7c67bedc145db1f6588809fb901d6eb2f2929437
arkarsoe <arkarsoe.passiongeek@gmail.com>
2025-01-15 16:16:33 +0630

    feat: add layer crud

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,8 +1,28 @@
 import axios, { AxiosInstance } from "axios";
+import Cookies from "js-cookie";
 
 const http: AxiosInstance = axios.create({
     baseURL: `${import.meta.env.VITE_PUBLIC_APP_BASE_URL}/api/v1`,
     withCredentials: true
 })
+
+// Add a request interceptor to attach the bearer token
+http.interceptors.request.use(
+    (config) => {
+        // Retrieve the token from localStorage or another storage mechanism
+        const token = Cookies.get("token");
+
+        if (token) {
+            // Attach the token to the Authorization header
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        // Handle errors in request configuration
+        return Promise.reject(error);
+    }
+);
 
 export { http }
